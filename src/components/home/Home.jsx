7747b1239dca2d146bdc49647c82c6357ebf83db
terka80 <ww291891@gmail.com>
2025-10-23 07:06:51 +0300

    [Your CHANGE]

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './home.css'
-import home from"../../assets/home.svg"
+import home from"/assets/home.svg"
 export default function Home() {
   return <>
     <div className="d-flex flex-column  align-items-center justify-content-center text-center home text-white">
         <div className="text-center">
-            <img src="../../assets/home.svg" alt="home" className='mb-3'/>
+            <img src="/assets/home.svg" alt="home" className='mb-3'/>
         </div>
         <div className="text-center pt-4">
             <h2 className='text-uppercase mb-3 fs-1 fw-bolder'>start Framework</h2>
